a6fd1eafd5cdd7be5ee359a9b4e49a90e1d4f2b9
Chaoliang Zeng <ccllzeng@gmail.com>
2018-03-09 12:06:28 +0800

    change some expression

--- src/cpp_packages/test.cpp
+++ src/cpp_packages/test.cpp
@@ -3,6 +3,7 @@
 
 #include <folly/SocketAddress.h>
 #include <folly/io/async/AsyncSocket.h>
+#include <folly/io/async/EventBase.h>
 
 #include <iostream>
 
@@ -13,47 +14,43 @@ typedef std::function<void()> VoidCallback;
 class ConnCallback : public folly::AsyncSocket::ConnectCallback {
  public:
   ConnCallback()
-      : state(STATE_WAITING),
-        exception(folly::AsyncSocketException::UNKNOWN, "none") {}
+        :exception(folly::AsyncSocketException::UNKNOWN, "none") {}
 
   void connectSuccess() noexcept override {
-    state = STATE_SUCCEEDED;
     if (successCallback) {
       successCallback();
     }
   }
 
   void connectErr(const folly::AsyncSocketException& ex) noexcept override {
-    state = STATE_FAILED;
     exception = ex;
     if (errorCallback) {
       errorCallback();
     }
   }
 
-  StateEnum state;
   folly::AsyncSocketException exception;
   VoidCallback successCallback;
   VoidCallback errorCallback;
 };
 
-int main(){
-	IPCSocketPoolContext context = new IPCSocketPoolContext();
-	IPCServer server = new IPCServer(context);
+int main(int argc, char** argv){
+	IPCSocketPoolContext context;
+	IPCServer server(context);
 
 	folly::SocketAddress addr = folly::SocketAddress::makeFromPath("server_socket");
 	std::vector<folly::SocketAddress> a = {addr};
 
-	server.bind(addr);
+	server.bind(a);
 	std::thread t([&](){
 		server.start();
 	});
 
-	EventBase evb;
-	std::shared_ptr<AsyncSocket> socket = AsyncSocket::newSocket(&evb);
+	folly::EventBase evb;
+	std::shared_ptr<folly::AsyncSocket> socket = folly::AsyncSocket::newSocket(&evb);
 	ConnCallback cb;
-	cb.successCallback = [](){std::cout<<"client success\n"};
-	cb.errorCallback = [](){std::cout<<"client fail\n"};
+	cb.successCallback = [](){std::cout<<"client success\n";};
+	cb.errorCallback = [](){std::cout<<"client fail\n";};
 	socket->connect(&cb,addr,30);
 	evb.loop();
 
