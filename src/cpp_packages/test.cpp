f2108f1c7011ee5ca4ab30a9152ba5f858bc37b9
HydraZeng <ccllzeng@gmail.com>
2018-03-09 11:50:11 +0800

    fix bugs in test file

--- src/cpp_packages/test.cpp
+++ src/cpp_packages/test.cpp
@@ -3,11 +3,39 @@
 
 #include <folly/SocketAddress.h>
 #include <folly/io/async/AsyncSocket.h>
-#include <folly/io/async/test/AsyncSocketTest.h>
 
 #include <iostream>
 
 using singaistorageipc;
+
+typedef std::function<void()> VoidCallback;
+
+class ConnCallback : public folly::AsyncSocket::ConnectCallback {
+ public:
+  ConnCallback()
+      : state(STATE_WAITING),
+        exception(folly::AsyncSocketException::UNKNOWN, "none") {}
+
+  void connectSuccess() noexcept override {
+    state = STATE_SUCCEEDED;
+    if (successCallback) {
+      successCallback();
+    }
+  }
+
+  void connectErr(const folly::AsyncSocketException& ex) noexcept override {
+    state = STATE_FAILED;
+    exception = ex;
+    if (errorCallback) {
+      errorCallback();
+    }
+  }
+
+  StateEnum state;
+  folly::AsyncSocketException exception;
+  VoidCallback successCallback;
+  VoidCallback errorCallback;
+};
 
 int main(){
 	IPCSocketPoolContext context = new IPCSocketPoolContext();
@@ -23,7 +51,7 @@ int main(){
 
 	EventBase evb;
 	std::shared_ptr<AsyncSocket> socket = AsyncSocket::newSocket(&evb);
-	folly::AsyncSocketTest::ConnCallback cb;
+	ConnCallback cb;
 	cb.successCallback = [](){std::cout<<"client success\n"};
 	cb.errorCallback = [](){std::cout<<"client fail\n"};
 	socket->connect(&cb,addr,30);
