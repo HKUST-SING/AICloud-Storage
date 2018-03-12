a54d0d27232709d474d79a02310f6e1ad64a2a88
HydraZeng <ccllzeng@gmail.com>
2018-03-12 22:06:22 +0800

    add a test file

--- src/cpp_packages/test/test2.cpp
+++ src/cpp_packages/test/test2.cpp
@@ -5,6 +5,8 @@
 
 #include <iostream>
 #include <cstring>
+#include <unordered_map>
+#include <queue>
 
 #include "../lib/message/IPCAuthenticationMessage.h"
 #include "../lib/message/IPCConnectionReplyMessage.h"
@@ -15,16 +17,18 @@
 using namespace singaistorageipc;
 
 void insert(
-	std::unordered_map<std::string,std::queue<IPCReadRequestMessage>> map,
+	std::unordered_map<std::string,std::queue<IPCReadRequestMessage>> *map,
 	IPCReadRequestMessage msg){
-	auto search = map.find(msg.getPath());
-	if(search == map.end()){
-		std::queue<IPCReadRequestMessage queue;
+	auto search = map->find(msg.getPath());
+	if(search == map->end()){
+		std::queue<IPCReadRequestMessage> queue;
 		queue.emplace(msg);
-		map.emplace(msg,queue);
+		map->emplace(msg.getPath(),queue);
 	}
 	else{
 		search->second.emplace(msg);
+		map->erase(msg.getPath());
+		map->emplace(msg.getPath(),search->second);
 	}
 }
 
@@ -36,7 +40,7 @@ int main(int argc, char** argv){
 	r2.setPath("a2");
 	r3.setPath("a1");
 
-	insert(map,r1);
+	insert(&map,r1);
 	auto search = map.find(r1.getPath());
 	if(search != map.end()){
 		std::cout << "insert r1 successfully: " 
@@ -46,8 +50,8 @@ int main(int argc, char** argv){
 		std::cout << "insert r1 failed" << std::endl;
 	}
 
-	insert(map,r2);
-	auto search = map.find(r2.getPath());
+	insert(&map,r2);
+	search = map.find(r2.getPath());
 	if(search != map.end()){
 		std::cout << "insert r2 successfully: " 
 			<< search->second.size() <<std::endl;
@@ -56,8 +60,8 @@ int main(int argc, char** argv){
 		std::cout << "insert r2 failed" << std::endl;
 	}
 
-	insert(map,r3);
-	auto search = map.find(r3.getPath());
+	insert(&map,r3);
+	search = map.find(r3.getPath());
 	if(search != map.end()){
 		std::cout << "insert r3 successfully: " 
 			<< search->second.size() <<std::endl;
@@ -65,6 +69,14 @@ int main(int argc, char** argv){
 	else{
 		std::cout << "insert r3 failed" << std::endl;
 	}
+
+	search = map.find(r3.getPath());
+	IPCReadRequestMessage r = search->second.front();
+	std::cout << r.getPath() << std::endl;
+	search->second.pop();
+	
+	search = map.find(r3.getPath());
+	std::cout << search->second.size() << std::endl;
 
 
 	return 0;
