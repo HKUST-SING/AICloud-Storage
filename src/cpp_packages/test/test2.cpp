a64cbcaafd305ae3185059e5907067724fe80fa6
HydraZeng <ccllzeng@gmail.com>
2018-03-12 21:46:14 +0800

    add a test file

--- src/cpp_packages/test/test2.cpp
+++ src/cpp_packages/test/test2.cpp
@@ -12,64 +12,60 @@
 #include "../lib/message/IPCWriteRequestMessage.h"
 #include "../lib/message/IPCStatusMessage.h"
 
-//using namespace singaistorageipc;
-
-//typedef std::function<void()> VoidCallback;
-/*
-class ConnCallback : public folly::AsyncSocket::ConnectCallback {
- public:
-  ConnCallback()
-        :exception(folly::AsyncSocketException::UNKNOWN, "none") {}
-
-  void connectSuccess() noexcept override {
-    if (successCallback) {
-      successCallback();
-    }
-  }
-
-  void connectErr(const folly::AsyncSocketException& ex) noexcept override {
-    exception = ex;
-    if (errorCallback) {
-      errorCallback();
-    }
-  }
-
-  folly::AsyncSocketException exception;
-  VoidCallback successCallback;
-  VoidCallback errorCallback;
-};
-*/
-
-void setUsername(const std::string& name){
-	std::string a = name;
-	std::cout << a << std::endl;
-};
-
 using namespace singaistorageipc;
 
-int main(int argc, char** argv){
-	//IPCSocketPoolContext context;
-	//IPCServer server(context);
-	IPCAuthenticationMessage msg;
-	IPCConnectionReplyMessage msg2;
-	IPCReadRequestMessage msg3;
-	IPCWriteRequestMessage msg4;
-	IPCStatusMessage msg5;
-	std::cout << "finish" << std::endl;
-	//std::vector<folly::SocketAddress> a = {addr};
-/*
-	server.bind(a);
-	std::thread t([&](){
-		server.start();
-	});
+void insert(
+	std::unordered_map<std::string,std::queue<IPCReadRequestMessage>> map,
+	IPCReadRequestMessage msg){
+	auto search = map.find(msg.getPath());
+	if(search == map.end()){
+		std::queue<IPCReadRequestMessage queue;
+		queue.emplace(msg);
+		map.emplace(msg,queue);
+	}
+	else{
+		search->second.emplace(msg);
+	}
+}
 
-	folly::EventBase evb;
-	std::shared_ptr<folly::AsyncSocket> socket = folly::AsyncSocket::newSocket(&evb);
-	ConnCallback cb;
-	cb.successCallback = [](){std::cout<<"client success\n";};
-	cb.errorCallback = [](){std::cout<<"client fail\n";};
-	socket->connect(&cb,addr,30);
-	evb.loop();
-*/
+int main(int argc, char** argv){
+	std::unordered_map<std::string,std::queue<IPCReadRequestMessage>> map;
+
+	IPCReadRequestMessage r1,r2,r3;
+	r1.setPath("a1");
+	r2.setPath("a2");
+	r3.setPath("a1");
+
+	insert(map,r1);
+	auto search = map.find(r1.getPath());
+	if(search != map.end()){
+		std::cout << "insert r1 successfully: " 
+			<< search->second.size() <<std::endl;
+	}
+	else{
+		std::cout << "insert r1 failed" << std::endl;
+	}
+
+	insert(map,r2);
+	auto search = map.find(r2.getPath());
+	if(search != map.end()){
+		std::cout << "insert r2 successfully: " 
+			<< search->second.size() <<std::endl;
+	}
+	else{
+		std::cout << "insert r2 failed" << std::endl;
+	}
+
+	insert(map,r3);
+	auto search = map.find(r3.getPath());
+	if(search != map.end()){
+		std::cout << "insert r3 successfully: " 
+			<< search->second.size() <<std::endl;
+	}
+	else{
+		std::cout << "insert r3 failed" << std::endl;
+	}
+
+
 	return 0;
 }
