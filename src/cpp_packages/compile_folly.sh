4f51a52fd97a7208542180c8d67ab1323855ecbd
HydraZeng <ccllzeng@gmail.com>
2018-03-15 14:18:40 +0800

    modify the script: remove all the argument

--- src/cpp_packages/compile_folly.sh
+++ src/cpp_packages/compile_folly.sh
@@ -8,6 +8,7 @@ if [ $# -eq 0 ]; then
   echo "No arguments supplied"
 fi
 
-output=$(echo $1|cut -d "." -f 1)
+#output=$(echo $1|cut -d "." -f 1)
 
-g++ -std=c++14 "$@" -lssl -lcrypto -lrt -lfolly -lpthread -lglog -levent -ldl -ldouble-conversion -o $output
+#g++ -std=c++14 "$@" -lssl -lcrypto -lrt -lfolly -lpthread -lglog -levent -ldl -ldouble-conversion -o $output
+g++ -std=c++14 main.cpp IPCServer.cpp lib/callback/*.cpp lib/utils/*.cpp -lssl -lcrypto -lrt -lfolly -lpthread -lglog -levent -ldl -ldouble-conversion -o main
