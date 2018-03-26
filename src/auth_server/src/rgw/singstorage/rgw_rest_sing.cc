13de03e7644c3390934f03a01671cc5f390cfb24
HydraZeng <ccllzeng@gmail.com>
2018-03-26 21:38:00 +0800

    define our own rest handler

---
+++ src/auth_server/src/rgw/singstorage/rgw_rest_sing.cc
@@ -0,0 +1,339 @@
+// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
+// vim: ts=8 sw=2 smarttab
+
+#include <boost/algorithm/string/predicate.hpp>
+#include <boost/format.hpp>
+#include <boost/optional.hpp>
+#include <boost/utility/in_place_factory.hpp>
+
+#include "include/assert.h"
+#include "ceph_ver.h"
+
+#include "common/Formatter.h"
+#include "common/utf8.h"
+#include "common/ceph_json.h"
+
+#include "rgw_rest_sing.h"
+#include "rgw_acl_sing.h"
+#include "rgw_cors_sing.h"
+#include "rgw_formats.h"
+#include "rgw_client_io.h"
+
+#include "rgw_auth.h"
+#include "rgw_sing_auth.h"
+
+#include "rgw_request.h"
+#include "rgw_process.h"
+
+#include <array>
+#include <sstream>
+#include <memory>
+
+#include <boost/utility/string_ref.hpp>
+
+int RGWHandler_REST_SING::authorize()
+{
+  return rgw::auth::Strategy::apply(auth_strategy, s);
+}
+
+int RGWHandler_REST_SING::postauth_init()
+{
+  struct req_init_state* t = &s->init_state;
+
+  /* XXX Stub this until Sing Auth sets account into URL. */
+  s->bucket_tenant = s->user->user_id.tenant;
+  s->bucket_name = t->url_bucket;
+
+  dout(10) << "s->object=" <<
+    (!s->object.empty() ? s->object : rgw_obj_key("<NULL>"))
+           << " s->bucket="
+     << rgw_make_bucket_entry_name(s->bucket_tenant, s->bucket_name)
+     << dendl;
+
+  int ret;
+  ret = rgw_validate_tenant_name(s->bucket_tenant);
+  if (ret)
+    return ret;
+  ret = validate_bucket_name(s->bucket_name);
+  if (ret)
+    return ret;
+  ret = validate_object_name(s->object.name);
+  if (ret)
+    return ret;
+
+  if (!t->src_bucket.empty()) {
+    /*
+     * We don't allow cross-tenant copy at present. It requires account
+     * names in the URL for Sing.
+     */
+    s->src_tenant_name = s->user->user_id.tenant;
+    s->src_bucket_name = t->src_bucket;
+
+    ret = validate_bucket_name(s->src_bucket_name);
+    if (ret < 0) {
+      return ret;
+    }
+    ret = validate_object_name(s->src_object.name);
+    if (ret < 0) {
+      return ret;
+    }
+  }
+
+  return 0;
+}
+
+int RGWHandler_REST_SING::validate_bucket_name(const string& bucket)
+{
+  const size_t len = bucket.size();
+
+  if (len > MAX_BUCKET_NAME_LEN) {
+    /* Bucket Name too long. Generate custom error message and bind it
+     * to an R-value reference. */
+    const auto msg = boost::str(
+      boost::format("Container name length of %lld longer than %lld")
+        % len % int(MAX_BUCKET_NAME_LEN));
+    set_req_state_err(s, ERR_INVALID_BUCKET_NAME, msg);
+    return -ERR_INVALID_BUCKET_NAME;
+  }
+
+  const auto ret = RGWHandler_REST::validate_bucket_name(bucket);
+  if (ret < 0) {
+    return ret;
+  }
+
+  if (len == 0)
+    return 0;
+
+  if (bucket[0] == '.')
+    return -ERR_INVALID_BUCKET_NAME;
+
+  if (check_utf8(bucket.c_str(), len))
+    return -ERR_INVALID_UTF8;
+
+  const char *s = bucket.c_str();
+
+  for (size_t i = 0; i < len; ++i, ++s) {
+    if (*(unsigned char *)s == 0xff)
+      return -ERR_INVALID_BUCKET_NAME;
+  }
+
+  return 0;
+}
+
+static void next_tok(string& str, string& tok, char delim)
+{
+  if (str.size() == 0) {
+    tok = "";
+    return;
+  }
+  tok = str;
+  int pos = str.find(delim);
+  if (pos > 0) {
+    tok = str.substr(0, pos);
+    str = str.substr(pos + 1);
+  } else {
+    str = "";
+  }
+}
+
+int RGWHandler_REST_SING::init_from_header(struct req_state* const s,
+                                            const std::string& frontend_prefix)
+{
+  string req;
+  string first;
+
+  s->prot_flags |= RGW_REST_SING;
+
+  char reqbuf[frontend_prefix.length() + s->decoded_uri.length() + 1];
+  sprintf(reqbuf, "%s%s", frontend_prefix.c_str(), s->decoded_uri.c_str());
+  const char *req_name = reqbuf;
+
+  const char *p;
+
+  if (*req_name == '?') {
+    p = req_name;
+  } else {
+    p = s->info.request_params.c_str();
+  }
+
+  s->info.args.set(p);
+  s->info.args.parse();
+
+  /* Skip the leading slash of URL hierarchy. */
+  if (req_name[0] != '/') {
+    return 0;
+  } else {
+    req_name++;
+  }
+
+  if ('\0' == req_name[0]) {
+    return g_conf->rgw_sing_url_prefix == "/" ? -ERR_BAD_URL : 0;
+  }
+
+  req = req_name;
+
+  size_t pos = req.find('/');
+  if (std::string::npos != pos && g_conf->rgw_sing_url_prefix != "/") {
+    bool cut_url = g_conf->rgw_sing_url_prefix.length();
+    first = req.substr(0, pos);
+
+    if (first.compare(g_conf->rgw_sing_url_prefix) == 0) {
+      if (cut_url) {
+        /* Rewind to the "v1/..." part. */
+        next_tok(req, first, '/');
+      }
+    }
+  } else if (req.compare(g_conf->rgw_sing_url_prefix) == 0) {
+    s->formatter = new RGWFormatter_Plain;
+    return -ERR_BAD_URL;
+  } else {
+    first = req;
+  }
+
+  std::string tenant_path;
+  if (! g_conf->rgw_sing_tenant_name.empty()) {
+    tenant_path = "/AUTH_";
+    tenant_path.append(g_conf->rgw_sing_tenant_name);
+  }
+
+  /* verify that the request_uri conforms with what's expected */
+  char buf[g_conf->rgw_sing_url_prefix.length() + 16 + tenant_path.length()];
+  int blen;
+  if (g_conf->rgw_sing_url_prefix == "/") {
+    blen = sprintf(buf, "/v1%s", tenant_path.c_str());
+  } else {
+    blen = sprintf(buf, "/%s/v1%s",
+                   g_conf->rgw_sing_url_prefix.c_str(), tenant_path.c_str());
+  }
+
+  if (strncmp(reqbuf, buf, blen) != 0) {
+    return -ENOENT;
+  }
+
+  int ret = allocate_formatter(s, RGW_FORMAT_PLAIN, true);
+  if (ret < 0)
+    return ret;
+
+  string ver;
+
+  next_tok(req, ver, '/');
+
+  if (!tenant_path.empty() || g_conf->rgw_sing_account_in_url) {
+    string account_name;
+    next_tok(req, account_name, '/');
+
+    /* Erase all pre-defined prefixes like "AUTH_" or "KEY_". */
+    const vector<string> skipped_prefixes = { "AUTH_", "KEY_" };
+
+    for (const auto pfx : skipped_prefixes) {
+      const size_t comp_len = min(account_name.length(), pfx.length());
+      if (account_name.compare(0, comp_len, pfx) == 0) {
+        /* Prefix is present. Drop it. */
+        account_name = account_name.substr(comp_len);
+        break;
+      }
+    }
+
+    if (account_name.empty()) {
+      return -ERR_PRECONDITION_FAILED;
+    } else {
+      s->account_name = account_name;
+    }
+  }
+
+  next_tok(req, first, '/');
+
+  dout(10) << "ver=" << ver << " first=" << first << " req=" << req << dendl;
+  if (first.size() == 0)
+    return 0;
+
+  s->info.effective_uri = "/" + first;
+
+  // Save bucket to tide us over until token is parsed.
+  s->init_state.url_bucket = first;
+
+  if (req.size()) {
+    s->object =
+      rgw_obj_key(req, s->info.env->get("HTTP_X_OBJECT_VERSION_ID", "")); /* rgw sing extension */
+    s->info.effective_uri.append("/" + s->object.name);
+  }
+
+  return 0;
+}
+
+int RGWHandler_REST_SING::init(RGWRados* store, struct req_state* s,
+        rgw::io::BasicClient *cio)
+{
+  struct req_init_state *t = &s->init_state;
+
+  s->dialect = "sing";
+
+  std::string copy_source =
+    url_decode(s->info.env->get("HTTP_X_COPY_FROM", ""));
+  if (! copy_source.empty()) {
+    bool result = RGWCopyObj::parse_copy_location(copy_source, t->src_bucket,
+              s->src_object);
+    if (!result)
+      return -ERR_BAD_URL;
+  }
+
+  if (s->op == OP_COPY) {
+    std::string req_dest =
+      url_decode(s->info.env->get("HTTP_DESTINATION", ""));
+    if (req_dest.empty())
+      return -ERR_BAD_URL;
+
+    std::string dest_bucket_name;
+    rgw_obj_key dest_obj_key;
+    bool result =
+      RGWCopyObj::parse_copy_location(req_dest, dest_bucket_name,
+              dest_obj_key);
+    if (!result)
+       return -ERR_BAD_URL;
+
+    std::string dest_object = dest_obj_key.name;
+
+    /* convert COPY operation into PUT */
+    t->src_bucket = t->url_bucket;
+    s->src_object = s->object;
+    t->url_bucket = dest_bucket_name;
+    s->object = rgw_obj_key(dest_object);
+    s->op = OP_PUT;
+  }
+
+  return RGWHandler_REST::init(store, s, cio);
+}
+
+RGWHandler_REST*
+RGWRESTMgr_SING::get_handler(struct req_state* const s,
+                              const rgw::auth::StrategyRegistry& auth_registry,
+                              const std::string& frontend_prefix)
+{
+  int ret = RGWHandler_REST_SING::init_from_header(s, frontend_prefix);
+  if (ret < 0) {
+    ldout(s->cct, 10) << "init_from_header returned err=" << ret <<  dendl;
+    return nullptr;
+  }
+
+  const auto& auth_strategy = auth_registry.get_sing();
+
+  if (s->init_state.url_bucket.empty()) {
+    return new RGWHandler_REST_Service_SING(auth_strategy);
+  }
+
+  if (s->object.empty()) {
+    return new RGWHandler_REST_Bucket_SING(auth_strategy);
+  }
+
+  return new RGWHandler_REST_Obj_SING(auth_strategy);
+}
+
+RGWHandler_REST* RGWRESTMgr_SING_Info::get_handler(
+  struct req_state* const s,
+  const rgw::auth::StrategyRegistry& auth_registry,
+  const std::string& frontend_prefix)
+{
+  s->prot_flags |= RGW_REST_SING;
+  const auto& auth_strategy = auth_registry.get_sing();
+  return new RGWHandler_REST_SING_Info(auth_strategy);
+}
---
+++ src/auth_server/src/rgw/singstorage/rgw_rest_sing.h
@@ -0,0 +1,105 @@
+// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
+// vim: ts=8 sw=2 smarttab
+
+#ifndef CEPH_RGW_REST_SING_H
+#define CEPH_RGW_REST_SING_H
+#define TIME_BUF_SIZE 128
+
+#include <boost/optional.hpp>
+#include <boost/utility/typed_in_place_factory.hpp>
+
+#include "rgw_op.h"
+#include "rgw_rest.h"
+#include "rgw_sing_auth.h"
+#include "rgw_http_errors.h"
+
+#include <boost/utility/string_ref.hpp>
+
+class RGWHandler_REST_SING : public RGWHandler_REST {
+  friend class RGWRESTMgr_SING;
+  friend class RGWRESTMgr_SING_Info;
+protected:
+  const rgw::auth::Strategy& auth_strategy;
+
+  virtual bool is_acl_op() {
+    return false;
+  }
+
+  static int init_from_header(struct req_state* s,
+                              const std::string& frontend_prefix);
+public:
+  RGWHandler_REST_SING(const rgw::auth::Strategy& auth_strategy)
+    : auth_strategy(auth_strategy) {
+  }
+  ~RGWHandler_REST_SING() override = default;
+
+  int validate_bucket_name(const string& bucket);
+
+  int init(RGWRados *store, struct req_state *s, rgw::io::BasicClient *cio) override;
+  int authorize() override;
+  int postauth_init() override;
+
+  RGWAccessControlPolicy *alloc_policy() { return nullptr; /* return new RGWAccessControlPolicy_SING; */ }
+  void free_policy(RGWAccessControlPolicy *policy) { delete policy; }
+};
+
+class RGWRESTMgr_SING : public RGWRESTMgr {
+protected:
+  RGWRESTMgr* get_resource_mgr_as_default(struct req_state* const s,
+                                          const std::string& uri,
+                                          std::string* const out_uri) override {
+    return this->get_resource_mgr(s, uri, out_uri);
+  }
+
+public:
+  RGWRESTMgr_SING() = default;
+  ~RGWRESTMgr_SING() override = default;
+
+  RGWHandler_REST *get_handler(struct req_state *s,
+                               const rgw::auth::StrategyRegistry& auth_registry,
+                               const std::string& frontend_prefix) override;
+};
+
+class RGWHandler_REST_SING_Info : public RGWHandler_REST_SING {
+public:
+  using RGWHandler_REST_SING::RGWHandler_REST_SING;
+  ~RGWHandler_REST_SING_Info() override = default;
+
+  RGWOp *op_get() override {
+    return new RGWInfo_ObjStore_SING();
+  }
+
+  int init(RGWRados* const store,
+           struct req_state* const state,
+           rgw::io::BasicClient* const cio) override {
+    state->dialect = "sing";
+    state->formatter = new JSONFormatter;
+    state->format = RGW_FORMAT_JSON;
+
+    return RGWHandler::init(store, state, cio);
+  }
+
+  int authorize() override {
+    return 0;
+  }
+
+  int postauth_init() override {
+    return 0;
+  }
+
+  int read_permissions(RGWOp *) override {
+    return 0;
+  }
+};
+
+class RGWRESTMgr_SING_Info : public RGWRESTMgr {
+public:
+  RGWRESTMgr_SING_Info() = default;
+  ~RGWRESTMgr_SING_Info() override = default;
+
+  RGWHandler_REST *get_handler(struct req_state* s,
+                               const rgw::auth::StrategyRegistry& auth_registry,
+                               const std::string& frontend_prefix) override;
+};
+
+#endif
